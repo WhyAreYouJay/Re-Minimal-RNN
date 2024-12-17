5483a56e41eec7bacb586fa8d02146e2112726c2
WhyAreYouJay <117834927+WhyAreYouJay@users.noreply.github.com>
2024-12-17 23:36:59 +0100

    Update eval.py

--- eval.py
+++ eval.py
@@ -19,7 +19,7 @@ def Reinformer_eval(
 
     state_dim = env.observation_space.shape[0]
     act_dim = env.action_space.shape[0]
-    env.set_np_random_through_seed(seed)
+    env.reset(seed = seed)
     state_mean = torch.from_numpy(state_mean).to(device)
     state_std = torch.from_numpy(state_std).to(device)
 
