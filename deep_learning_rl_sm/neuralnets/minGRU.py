00efe9a966c61ebe1c52d5b9b69f04494b325010
WhyAreYouJay <117834927+WhyAreYouJay@users.noreply.github.com>
2024-12-19 07:18:50 +0100

    Update minGRU.py

--- deep_learning_rl_sm/neuralnets/minGRU.py
+++ deep_learning_rl_sm/neuralnets/minGRU.py
@@ -21,8 +21,6 @@ def parallel_scan_log(log_coefficients, log_values):
     a_star = F.pad(torch.cumsum(log_coefficients, dim=1), (0, 0, 1, 0))
     log_h0_plus_b_star = torch.logcumsumexp(log_values - a_star, dim=1)
     log_h = a_star + log_h0_plus_b_star
-    del log_coefficients,log_values
-    torch.cuda.empty_cache()
     return torch.exp(log_h)[:, 1:]
 
 class Conv1dLayer(Module):
