c5ee741087654a9ee6d3112ed3a5c07366946612
WhyAreYouJay <117834927+WhyAreYouJay@users.noreply.github.com>
2024-12-21 15:41:16 +0100

    Update train_benchmark.py

--- train_benchmark.py
+++ train_benchmark.py
@@ -55,7 +55,7 @@ def parse_args():
     parser.add_argument("--env", choices=["halfcheetah", "walker2d", "hopper"], default="halfcheetah")
     parser.add_argument("--env_discrete", type=bool, default=False)
     parser.add_argument("--dataset", choices=["medium", "medium_expert", "medium_replay"], type=str, default="medium")
-    parser.add_argument("--num_eval_ep", type=int, default=3)
+    parser.add_argument("--num_eval_ep", type=int, default=10)
     parser.add_argument("--max_eval_ep_len", type=int, default=1000)
     parser.add_argument("--dataset_dir", type=str,
                         default="deep_learning_rl_sm/benchmarks/data/halfcheetah_medium_expert-v2.hdf5")
