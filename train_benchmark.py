ab05848e59430d55f4241e8e8b31248d357300f1
chrishon <chrishon.nilanthan@gmail.com>
2024-12-22 11:32:10 +0100

    fix

--- train_benchmark.py
+++ train_benchmark.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
     Ks = [5,20]
     batch_sizes = [64,128]
     lrs = [10**-4,10**-3]
-    taus = {"halfcheetah":{"medium": 0.9, "medium-replay": 0.9, "medium-expert": 0.9},"hopper":{"medium": 0.999, "medium-replay": 0.999, "medium-expert": 0.9},"walker2d":{"medium": 0.9, "medium-replay": 0.99, "medium-expert": 0.99}}
+    taus = {"halfcheetah":{"medium": 0.9, "medium_replay": 0.9, "medium_expert": 0.9},"hopper":{"medium": 0.999, "medium_replay": 0.999, "medium_expert": 0.9},"walker2d":{"medium": 0.9, "medium_replay": 0.99, "medium_expert": 0.99}}
 
     
     args = parse_args()
