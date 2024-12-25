7b9c5d7f50e327c42187f549e852df3dd019a25a
chrishon <chrishon.nilanthan@gmail.com>
2024-12-25 10:30:41 +0100

    reduce hyperparams

--- train_benchmark.py
+++ train_benchmark.py
@@ -88,8 +88,8 @@ def parse_args():
 
 if __name__ == "__main__":
     seeds = [0,42,2024]
-    Ks = [5,20]
-    batch_sizes = [64,128]
+    Ks = [20]
+    batch_sizes = [128]
     lrs = [10**-3]
     taus = {"halfcheetah":{"medium": 0.9, "medium_replay": 0.9, "medium_expert": 0.9},"hopper":{"medium": 0.999, "medium_replay": 0.999, "medium_expert": 0.9},"walker2d":{"medium": 0.9, "medium_replay": 0.99, "medium_expert": 0.99}}
     total_runs = len(seeds) * len(Ks) * len(batch_sizes) * len(lrs)
