0233b83e3f3f62b4d1334cadc32eece8c13dcdfe
rajensen <rajensen@ethz.ch>
2025-01-02 22:06:27 +0100

    .

--- deep_learning_rl_sm/neuralnets/minGRU.py
+++ deep_learning_rl_sm/neuralnets/minGRU.py
@@ -73,7 +73,7 @@ class minGRU(Module):
     def forward(self, x:torch.Tensor, h0=None):
         # x: (batch_size, seq_len, hidden_size)
         # h_0: (batch_size, 1, hidden_size)
-        k,h_x = self.f(x).chunk(2,dim = -1)
+        k,h_x = self.drop_f(self.f(x)).chunk(2,dim = -1)
         log_z = -F.softplus(-k)
         log_coeffs = -F.softplus(k)
         log_tilde_h = log_g(h_x)
